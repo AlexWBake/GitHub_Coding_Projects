644ed3f05bb4319c6e1497c7d7a53538e0141160
Alex Baker <104365786+AlexWBake@users.noreply.github.com>
2025-11-19 22:54:35 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int myFavoriteNumber = 0;
+
+    printf("Enter Favorite Number\n");
+    scanf("%d", &myFavoriteNumber);
+    printf("You entered the number: %d\n", myFavoriteNumber);
+    
+    return 0;
+}
